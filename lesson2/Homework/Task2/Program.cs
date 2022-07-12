48faa859d2b851d90cb3f7a3445ae138774fe616
P1k4sh <p1k4sh@yandex.ru>
2022-07-12 12:17:32 +0300

    Hw_Task2 is resolved

---
+++ lesson2/Homework/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру
+// этого числа. Например, 456 -> 46, 782 -> 72, 918 -> 98
+
+int a = new Random().Next(100, 1000);
+Console.WriteLine($"Generated number is {a}");
+
+void deleteSecNum(int a)
+{
+    int first = a / 100;
+    int third = a % 10;
+    Console.WriteLine($"Answer is {first}{third}");
+}
+
+deleteSecNum(a);
+
