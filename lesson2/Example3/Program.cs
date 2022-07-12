057e004a8330d9f6a2824b3499d1e5b826e76949
P1k4sh <p1k4sh@yandex.ru>
2022-07-12 11:07:59 +0300

    Task3 resolved

---
+++ lesson2/Example3/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+void checking(int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("b кратно a");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно, остаток " + a % b);
+    }
+}
+
+checking(a, b);
+
