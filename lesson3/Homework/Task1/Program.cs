de228341f10cc42579a82263e3f9eb80454281a5
P1k4sh <p1k4sh@yandex.ru>
2022-07-15 13:54:58 +0300

    HW_Task1 resolved

---
+++ lesson3/Homework/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно
+// палиндромом. Например, 14212 -> нет, 23432 -> да, 12821 -> да
+
+Console.Write("Enter 5-digit number: ");
+int a = int.Parse(Console.ReadLine());
+
+void palindrom_check(int number)
+{
+    if (number > 99999 || number < 10000)
+    {
+        Console.WriteLine("Error. Write correct 5-digit number");
+        return;
+    }
+
+    while (true)
+    {
+        if (number % 10 == number / 10000)
+        {
+            if (number / 1000 % 10 == number % 100 / 10)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("No");
+            return;
+        }
+    }
+}
+
+palindrom_check(a);
+
