2e321f2b216b3c9dafdd9f3dbee3abf746d3558a
P1k4sh <p1k4sh@yandex.ru>
2022-08-02 10:44:03 +0300

    example1 resolved

--- lesson4/Example1/Program.cs
+++ lesson4/Example1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//Например, 7 -> 28, 4 -> 10,  8 -> 36
+
+int SumOfNumbers(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+Console.WriteLine(SumOfNumbers(7));
