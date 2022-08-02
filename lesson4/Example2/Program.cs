23fa90546d266135c915a95390f61dcd9ba4bd1d
P1k4sh <p1k4sh@yandex.ru>
2022-08-02 11:32:53 +0300

    Examples 2 & 3 resolved

--- lesson4/Example2/Program.cs
+++ lesson4/Example2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// Например, 4 -> 24, 5 -> 120
+
+int Fctorial(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+Console.WriteLine(Fctorial(4));
+Console.WriteLine(Fctorial(5));
+Console.WriteLine(Fctorial(6));
