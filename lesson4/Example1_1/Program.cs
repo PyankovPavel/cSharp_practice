1ffe292a2b37bc3f61aad9322f9d12f255717580
P1k4sh <p1k4sh@yandex.ru>
2022-08-02 11:03:53 +0300

    Example 1_1 resolved

--- lesson4/Example1_1/Program.cs
+++ lesson4/Example1_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// Например, 456 -> 3, 78 -> 2, 89126 -> 5
+
+int CheckNumbers(int a)
+{
+    int count = 0;
+    for (; a > 0;)
+    {
+        a = a/10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine(CheckNumbers(78));
