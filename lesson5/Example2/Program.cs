40fea36e760e6d793cb4f9c376efe1d361d8c412
P1k4sh <p1k4sh@yandex.ru>
2022-08-05 11:06:45 +0300

    task 2 resolved

---
+++ lesson5/Example2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве. Например:
+// 4; массив [6, 7, 19, 345, 3] -> нет, 
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] fillTheArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+void printTheArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+void findTheNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == num){
+            Console.WriteLine("Yes");
+            return;
+        }
+    }
+    Console.WriteLine("No");
+}
+
+Console.Write("Enter the number to find ");
+int find = int.Parse(Console.ReadLine());
+int[] array = fillTheArray(5);
+printTheArray(array);
+findTheNum(array, find);
+
