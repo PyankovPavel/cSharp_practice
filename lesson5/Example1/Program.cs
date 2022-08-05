3d6dc5dfab9d4dfe740736d8f805e1354774ce8c
P1k4sh <p1k4sh@yandex.ru>
2022-08-05 10:51:49 +0300

    task 1 resolved

--- lesson5/Example1/Program.cs
+++ lesson5/Example1/Program.cs
@@ -1,3 +1,37 @@
 ﻿// Задача 1: Напишите программу замена элементов массива: положительные элементы замените на соответствующие
 // отрицательные, и наоборот. Например,
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] fillTheArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+void printTheArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+
+}
+
+void changePostoNeg(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     array[i] *= -1;
+    }
+    printTheArray(array);
+}
+
+int[] array = fillTheArray(4);
+printTheArray(array);
+changePostoNeg(array);
