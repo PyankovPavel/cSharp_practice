be358593064083ffb9b66b82cf4b01f42c9b59a7
P1k4sh <p1k4sh@yandex.ru>
2022-08-07 00:17:52 +0300

    HW_Task2 is resolved

---
+++ lesson5/Homework/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях. Например,
+// [3, 7, 23, 12] -> 26
+// [-4, -6, 4, 67] -> 0
+
+int[] fillTheArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    return array;
+}
+
+void printTheArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int sumOfOddPos(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i=i+2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = fillTheArray(6);
+printTheArray(array);
+Console.WriteLine($"Sum of elements on odd positions: {sumOfOddPos(array)}");
