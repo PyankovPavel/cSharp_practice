b3c15bf35f2ff07677eb7ebe85acd8b345e84de8
P1k4sh <p1k4sh@yandex.ru>
2022-08-09 15:45:48 +0300

    Examle1_demo solved

--- lesson6/Example1_demo/Program.cs
+++ lesson6/Example1_demo/Program.cs
@@ -3,4 +3,41 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] fillTheArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
 
+void printTheArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int[] turnArray(int[] array)
+{
+    // int temp = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        // temp = array[i];
+        // array[i] = array[array.Length - i - 1]; // -> тоже рабочий код
+        // array[array.Length - i - 1] = temp;
+
+        (array[i], array[array.Length - i - 1]) = (array[array.Length - i - 1], array[i]); // -> метод "кортежей" с семинара
+    }
+
+    return array;
+}
+
+int[] array = fillTheArray(4);
+printTheArray(array);
+printTheArray(turnArray(array));
