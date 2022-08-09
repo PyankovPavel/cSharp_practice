6d35a6e677a8aad8888c59338685a3314b49a159
P1k4sh <p1k4sh@yandex.ru>
2022-08-09 17:01:57 +0300

    Examle2 solved

---
+++ lesson6/Example2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
+// Например: 45 -> 101101, 3 -> 11, 2 -> 10
+
+string transform(int a)
+{
+    String result = "";
+    while (a > 0)
+    {
+        result = a % 2 + result;
+        a /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine(transform(12));
+
+
+
