9af8984075817eeb4baccc1299a27a60c323de0c
P1k4sh <p1k4sh@yandex.ru>
2022-08-09 16:08:58 +0300

    Examle1 solved

---
+++ lesson6/Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// *** Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+void checkTriangle(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine("Exist");
+    }
+    else
+    {
+        Console.WriteLine("Not exist");
+    }
+}
+
+checkTriangle(5, 5, 5);
+checkTriangle(11, 5, 5);
+checkTriangle(1, 2, 3);
+checkTriangle(4, 2, 5);
