f338303e7e310bfcf93599bd77167209452cca95
P1k4sh <hsakip@gmail.com>
2022-08-09 23:37:00 +0300

    HW_Task2 resolved

---
+++ lesson6/Homework/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем. Например:
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void crossPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"CrossPoint coordinates:({x}, {y})");
+}
+
+crossPoint(2, 5, 4, 9);
+crossPoint(1, 0.5, 4, -1);
+
