5c5a271c43a0dd45affad5441deb75f421c33f64
P1k4sh <hsakip@gmail.com>
2022-08-10 00:10:30 +0300

    HW_Task3 resolved

---
+++ lesson6/Homework/Task3(4)/Program.cs
@@ -0,0 +1,35 @@
+﻿// 3: Напишите программу, которая будет создавать копию заданного массива(вещественные числа) с помощью поэлементного копирования.
+
+double[] fillTheArray(int n)
+{
+    double[] array = new double[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * (100 + 150) - 100, 2);
+    }
+    return array;
+}
+
+void printTheArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+double[] copyTheArray(double[] array)
+{
+    double[] new_arr = new double[array.Length];
+    for (int i = 0; i < new_arr.Length; i++)
+    {
+        new_arr[i] = array[i];
+    }
+    return new_arr;
+}
+
+double[] array = fillTheArray(5);
+printTheArray(array);
+printTheArray(copyTheArray(array));
