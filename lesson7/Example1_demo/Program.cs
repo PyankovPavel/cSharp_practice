138f193a5b8bb5e24e6894e70ccf45a9fe9cfd72
P1k4sh <p1k4sh@yandex.ru>
2022-08-12 10:26:00 +0300

    example1_demo resolved

--- lesson7/Example1_demo/Program.cs
+++ lesson7/Example1_demo/Program.cs
@@ -3,3 +3,30 @@
 // 1 4 8 19
 // 5 -2 33 -2
 // 77 3 8 1
+
+int[,] fillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-50, 101);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+printArray(fillArray(3, 4));
