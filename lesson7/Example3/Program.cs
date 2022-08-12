f92e88aa8e6b9f1df99a8d259ab7fafc4b2d3d10
P1k4sh <p1k4sh@yandex.ru>
2022-08-12 11:13:27 +0300

    example3 is resolved

---
+++ lesson7/Example3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 3: Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+// Например, задан массив:
+// 1 4 7
+// 5 9 2
+// 8 4 2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] fillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+
+int findSumOfDiagonalElements(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result += array[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = fillArray(4, 4);
+printArray(array);
+Console.WriteLine();
+Console.Write($"Sum of diagonal elements: {findSumOfDiagonalElements(array)}");
