3e2d2714a57bf244e0737a83da8a9622d1032d2d
P1k4sh <p1k4sh@yandex.ru>
2022-08-13 00:02:14 +0300

    Task2_HW is resolved

---
+++ lesson7/Homework/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание, что такого элемента нет. Напрмиер:
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int[,] fillArray()
+{
+    Console.Write("Enter number of rows: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Enter number of columns: ");
+    int n = int.Parse(Console.ReadLine());
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void findPositionOfElement(int[,] array)
+{
+    Console.WriteLine("Enter 1st index of position: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter 2nd index of position: ");
+    int b = int.Parse(Console.ReadLine());
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (a == i && b == j)
+            {
+                result = array[i, j];
+                Console.WriteLine($"Element on input position is {result} \v"); 
+                return;
+            }
+        }
+    }
+    Console.WriteLine("There is no element on that position \v");
+}
+
+int[,] array = fillArray();
+findPositionOfElement(array);
+printArray(array);
