52acbc712759e5ce7317fff07e8259bc75ed6835
P1k4sh <p1k4sh@yandex.ru>
2022-08-18 11:19:00 +0300

    HW_Task1 resolved

---
+++ lesson8/Homework/Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+int[,] fillArray()
+{
+    Console.Write("Enter number of rows: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Enter number of columns: ");
+    int n = int.Parse(Console.ReadLine());
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 50);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] minMaxArr(int[,] array)
+{
+    for (int i = array.GetLength(0) - 1; i > 0; i--)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[k, j] > array[k, j + 1])
+                {
+                    int tmp = array[k, j];
+                    array[k, j] = array[k, j + 1];
+                    array[k, j + 1] = tmp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = fillArray();
+printArray(array);
+Console.WriteLine();
+printArray(minMaxArr(array));
+
