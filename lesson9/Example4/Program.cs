d4dc828f7aadcabe58b7412681fe7dd477921226
P1k4sh <p1k4sh@yandex.ru>
2022-08-19 11:22:40 +0300

    Example4 resolved

---
+++ lesson9/Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+
+int exponent(int a, int b)
+{
+    if (b == 0) return 1;
+    return exponent(a, b - 1) * a;
+}
+
+Console.WriteLine(exponent(2, 3));
+Console.WriteLine(exponent(3, 5));
