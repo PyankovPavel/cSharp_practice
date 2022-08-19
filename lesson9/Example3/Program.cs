d08337974dcbad6da4342e7ebde8197e99b8bfc3
P1k4sh <p1k4sh@yandex.ru>
2022-08-19 11:15:47 +0300

    Example3 resolved

---
+++ lesson9/Example3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int sumOfNums(int num)
+{
+    if (num == 0) return 0;
+    return sumOfNums(num / 10) + num % 10;
+
+}
+Console.WriteLine(sumOfNums(123));
+Console.WriteLine(sumOfNums(45));
+
