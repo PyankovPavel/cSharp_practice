3b1b72124d0f06cd9f0eedf187182d0ac41a8889
P1k4sh <p1k4sh@yandex.ru>
2022-08-21 20:47:36 +0300

    Homework_Task1 resolved

--- lesson9/Homework/Task1/Program.cs
+++ lesson9/Homework/Task1/Program.cs
@@ -2,3 +2,27 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+void printNums(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    if (m % 2 == 0)
+    {
+        printNums(m, n - 2);
+        Console.WriteLine($"{n}");
+    }
+    else if(n > m)
+    {
+        n--;
+        printNums(m, n - 1);
+        Console.WriteLine($"{n}");
+    }
+}
+
+printNums(1, 5);
+Console.WriteLine();
+printNums(4, 8);
+Console.WriteLine();
+printNums(1, 15);
