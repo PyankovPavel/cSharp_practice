8b6156b219abbf97e28f4e27ee4a4767c88283b6
P1k4sh <p1k4sh@yandex.ru>
2022-08-28 18:43:25 +0300

    solved tasks 1&2

---
+++ additional_tasks/feel_yourself_intern/1/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+bool isSquare(int a, int b)
+{
+    return a == b * b;
+}
+
+Console.WriteLine(isSquare(25, 5));
+Console.WriteLine(isSquare(49, 7));
+Console.WriteLine(isSquare(10, 3));
---
+++ additional_tasks/feel_yourself_intern/2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  2. Даны два числа. Показать большее и меньшее число
+
+int[] maxMin(int a, int b)
+{
+    int[] array = new int[2];
+    if (a >= b)
+    {
+        array[0] = a;
+        array[1] = b;
+    }
+    else
+    {
+        array[0] = b;
+        array[1] = a;
+    }
+    return array;
+}
+
+string str = String.Join(", ", maxMin(2, 5));
+Console.WriteLine(str);
+string str1 = String.Join(", ", maxMin(100, 10));
+Console.WriteLine(str1);
