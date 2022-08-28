c226e8cc15aa9b1c3deaf35fbc34fd0f9123a64f
P1k4sh <hsakip@gmail.com>
2022-08-28 21:30:09 +0300

    Tasks 3,4 solved

---
+++ additional_tasks/feel_yourself_intern/3/Program.cs
@@ -0,0 +1,28 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+
+string dayName(int n)
+{
+    switch (n)
+    {
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednsday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
+        default:
+            return "Enter number from 1 to 7";
+    }
+}
+
+Console.WriteLine(dayName(5));
+Console.WriteLine(dayName(2));
+Console.WriteLine(dayName(15));
---
+++ additional_tasks/feel_yourself_intern/4/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4. Найти максимальное из трех чисел
+
+int Max(int a, int b, int c)
+{
+    int max = a;
+    if (b > a && b > c)
+    {
+        max = b;
+    }
+    else if (c > a && c > b)
+    {
+        max = c;
+    }
+    return max;
+}
+
+Console.WriteLine(Max(1, 2, 3));
+Console.WriteLine(Max(4, 1, 5));
+Console.WriteLine(Max(10, 20, 3));
