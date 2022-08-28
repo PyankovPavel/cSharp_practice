a8ad90a856aa73b9c751fd270515815c444a2f8e
P1k4sh <hsakip@gmail.com>
2022-08-29 00:37:03 +0300

    Tasks 7 & 8 solved

---
+++ additional_tasks/feel_yourself_intern/7/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7. Показать числа от -N до N
+
+int[] showNumbers(int number)
+{
+    int[] array = new int[number * 2 + 1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = number * -1;
+        number--;
+    }
+    return array;
+}
+
+string str = String.Join(", ", showNumbers(5));
+Console.WriteLine(str);
---
+++ additional_tasks/feel_yourself_intern/8/Program.cs
@@ -0,0 +1,16 @@
+﻿//  8. Показать четные числа от 1 до N
+
+int[] showNumbers(int number)
+{
+    int[] array = new int[number / 2];
+    int temp = 2;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = temp;
+        temp += 2;
+    }
+    return array;
+}
+
+string str = String.Join(", ", showNumbers(20));
+Console.WriteLine(str);
