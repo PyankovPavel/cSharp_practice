b0abb7ca862e8708cb6565e990731f4480983b88
P1k4sh <p1k4sh@yandex.ru>
2022-08-29 11:18:10 +0300

    solved tasks 11&12

---
+++ additional_tasks/feel_yourself_intern/11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int maxNumber()
+{
+    Random rnd = new Random();
+    int number = rnd.Next(10, 100);
+    Console.WriteLine(number);
+    int max = number / 10;
+    if (number % 10 > max)
+    {
+        max = number % 10;
+    }
+    return max;
+}
+
+Console.WriteLine(maxNumber());
---
+++ additional_tasks/feel_yourself_intern/12/Program.cs
@@ -0,0 +1,14 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int[] deleteMiddleNumber(int number)
+{
+    int[] array = new int[2];
+    array[0] = number / 100;
+    array[1] = number % 10;
+    return array;
+}
+
+string str = String.Join("", deleteMiddleNumber(123));
+Console.WriteLine(str);
+string str2 = String.Join("", deleteMiddleNumber(576));
+Console.WriteLine(str2);
