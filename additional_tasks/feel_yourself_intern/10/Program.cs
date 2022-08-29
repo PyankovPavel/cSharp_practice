51727c22b330f8b2a5536c3a932e7dc78cd3e004
P1k4sh <p1k4sh@yandex.ru>
2022-08-29 10:43:52 +0300

    solved tasks 9&10

---
+++ additional_tasks/feel_yourself_intern/10/Program.cs
@@ -0,0 +1,9 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+int middleNumber(int number)
+{
+    return number / 10 % 10;
+}
+
+Console.WriteLine(middleNumber(153));
+Console.WriteLine(middleNumber(567));
---
+++ additional_tasks/feel_yourself_intern/9/Program.cs
@@ -0,0 +1,9 @@
+﻿//  9. Показать последнюю цифру трёхзначного числа
+
+int lastNumber(int number)
+{
+    return number % 10;
+}
+
+Console.WriteLine(lastNumber(153));
+Console.WriteLine(lastNumber(567));
