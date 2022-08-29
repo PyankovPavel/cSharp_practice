7077c25956517debbcdcf9cbc2e67e29a9afe19e
P1k4sh <p1k4sh@yandex.ru>
2022-08-29 23:41:25 +0300

    solved tasks 13&14

---
+++ additional_tasks/feel_yourself_intern/13/Program.cs
@@ -0,0 +1,18 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+void isDivisible(int a, int b)
+{
+    bool isDiv = false;
+    if (a % b == 0)
+    {
+        isDiv = true;
+        Console.WriteLine(isDiv);
+    }
+    else
+    {
+        Console.WriteLine($"False, division surplus is {a % b}");
+    }
+}
+
+isDivisible(25,5);
+isDivisible(30,7);
---
+++ additional_tasks/feel_yourself_intern/14/Program.cs
@@ -0,0 +1,39 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+int thirdNumber(int number)
+{
+    int size = 1;
+    int temp = number;
+    while (temp / 10 > 0)
+    {
+        temp /= 10;
+        size++;
+    }
+    int[] array = new int[size];
+    int division = 1;
+    for (int i = 1; i < size; i++)
+    {
+        division *= 10;
+    }
+    for (int j = 0; j < array.Length; j++)
+    {
+        array[j] = number / division;
+        number %= division;
+        division /= 10;
+
+    }
+    if (size > 2)
+    {
+        return array[2];
+    }
+    else
+    {
+        Console.Write("No third number in input data. Error ");
+        return 404;
+    }
+
+}
+
+Console.WriteLine(thirdNumber(1234));
+Console.WriteLine(thirdNumber(10));
+
