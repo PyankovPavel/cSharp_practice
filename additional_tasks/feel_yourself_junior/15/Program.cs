4c7e96219ec6f95b8c2a2b6f41a4d2cb83f0f5bd
P1k4sh <p1k4sh@yandex.ru>
2022-09-01 14:48:41 +0300

    made feel_yourself_Junior folder & resolved 15&16 tasks

---
+++ additional_tasks/feel_yourself_junior/15/Program.cs
@@ -0,0 +1,10 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+bool isDivision(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+Console.WriteLine(isDivision(161));
+Console.WriteLine(isDivision(210));
+Console.WriteLine(isDivision(115));
---
+++ additional_tasks/feel_yourself_junior/16/Program.cs
@@ -0,0 +1,23 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+bool isWeekend(int number)
+{
+    bool flag = false;
+    if (number > 7)
+    {
+        Console.Write("Input number from 1 to 7, ");
+        return flag;
+    }
+    if (number > 0 && number < 6)
+    {
+        return flag;
+    }
+    else
+    {
+        return !flag;
+    }
+}
+
+Console.WriteLine(isWeekend(5));
+Console.WriteLine(isWeekend(6));
+Console.WriteLine(isWeekend(10));
